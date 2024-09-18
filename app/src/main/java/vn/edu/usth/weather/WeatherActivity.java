e6be5208f33e2507cf018981a9b59e824d296669
khôi <khoind.bi12-217@st.usth.edu.vn>
2024-09-18 23:10:31 +0700

    prac8+9

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -10,21 +10,30 @@ import androidx.core.view.WindowInsetsCompat;
 import android.util.Log;
 
 public class WeatherActivity extends AppCompatActivity {
-    private static final String TAG = "WeatherActivity";
-
+    private static final String TAG = "Weather";
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_weather);
-        //Create a new Fragment to be placed in the activity
-        ForecastFragment forecastFragment = new ForecastFragment();
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+        Log.i(TAG, "Create");
 
-        //Add the fragment to the 'container' FrameLayout
-        getSupportFragmentManager().beginTransaction().add(
-                R.id.fragment_forecast, forecastFragment).commit();
+        String[] cities = new String[]{"Hanoi, Vietnam", "Paris, France", "Melbourne, Australia"};
+        HomeFragmentPagerAdapter adapter = new HomeFragmentPagerAdapter(this);
+        ViewPager2 viewPager2 = findViewById(R.id.view_pager);
+        viewPager2.setOffscreenPageLimit(3);
+        viewPager2.setAdapter(adapter);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        new TabLayoutMediator(tabLayout, viewPager2, (tab, position) -> {
 
-        Log.i(TAG, "App Created");
-    }
+            tab.setText(cities[position]);
+        }).attach();
 
     @Override
     protected void onStart(){
--- app/src/main/java/vn/edu/usth/weather/WeatherAndForcastFragment.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherAndForcastFragment.java
@@ -11,19 +11,49 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class WeatherAndForcastFragment extends Fragment {
+
+    private String city_name;
+    private int weather_icon;
+
+    public WeatherAndForecastFragment() {
+        // Required empty public constructor
+    }
+
+    public static WeatherAndForecastFragment newInstance(String city, int weather_icon) {
+        WeatherAndForecastFragment fragment = new WeatherAndForecastFragment();
+        Bundle args = new Bundle();
+        args.putString("city", city);
+        args.putInt("weather_icon", weather_icon);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            city_name = getArguments().getString("city");
+            weather_icon = getArguments().getInt("weather_icon");
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_weather_and_forcast, container, false);
-
+        View view = inflater.inflate(R.layout.fragment_weather_and_forecast, container, false);
         FragmentManager fragmentManager = getChildFragmentManager();
 
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.weather_container, new WeatherFragment());
-        transaction.replace(R.id.forecast_container, new ForecastFragment());
-        transaction.commit();
+        WeatherFragment weatherFragment = new WeatherFragment();
 
+        Bundle bundle = new Bundle();
+        bundle.putString("city", city_name);
+        bundle.putInt("weather_icon", weather_icon);
+        weatherFragment.setArguments(bundle);
+
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_weather, weatherFragment)
+                .commit();
         return view;
     }
 }
